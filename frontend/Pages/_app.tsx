bf6d32f9a2f62e4f917afb51694775feed369d17
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-08-07 18:13:35 +0900

    🌱 Add _app, _error, index

---
+++ frontend/Pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app';
+import * as React from 'react';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ frontend/Pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h2>index page !!</h2>
+        </div>
+    );
+};
+
+export default Index;
